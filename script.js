9bea9958f8a58480ee5df43de4830443e8e39b7f
Arthur <arthurlol2131@gmail.com>
2024-09-08 13:24:59 -0300

    att 08/09

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const button = document.querySelector('.button-task')
 const input = document.querySelector('.input-task')
 const listaCompleta = document.querySelector('.list-task')
+
 let minhaListaDeItens = []
 
 
 function adicionarNovaTarefa() {
-    minhaListaDeItens.push(input.value)
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
 
     input.value = ''
 
@@ -18,9 +22,9 @@ function mostrarTarefas() {
 
     minhaListaDeItens.forEach((família, posicao) => {
         novaLi = novaLi + `   
-         <li class="task">
-        <img src="Componentes/checked.png" class="checked">
-        <p class="text">${família}</p>
+         <li class="task ${família.concluida && "done"}">
+        <img src="Componentes/checked.png" class="checked" onclick="concluirTarefa(${posicao})">
+        <p class="text">${família.tarefa}</p>
         <img src="Componentes/trash.png" class="trash" onclick="deletarItem(${posicao})">
         </li>
 
@@ -31,8 +35,11 @@ function mostrarTarefas() {
 
 }
 
-function concluirTarefa (){
-    
+
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
 }
 
 function deletarItem(posicao) {
